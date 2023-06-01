d4f586ebbb9f3cc9d65426c3f9ba1d9a31211b66
BraveSeeker <82483357+BraveSeeker@users.noreply.github.com>
2023-06-01 16:24:56 +0800

    Update Constant.java

--- 实验4 天气预报/WeatherForcast/entry/src/main/java/com/example/weatherforcast/constant/Constant.java
+++ 实验4 天气预报/WeatherForcast/entry/src/main/java/com/example/weatherforcast/constant/Constant.java
@@ -4,15 +4,15 @@ package com.example.weatherforcast.constant;
  * 常量
  */
 public class Constant {
-
+    String API_KEY = "your api key";
     // 今日天气
-    public static final String TODAY_URL = "https://api.seniverse.com/v3/weather/now.json?key=SM1jkVpoja_DPR9gF&location=%s&language=zh-Hans&unit=c";
+    public static final String TODAY_URL = String.format("https://api.seniverse.com/v3/weather/now.json?key={}&location=%s&language=zh-Hans&unit=c",API_KEY};
 
     // 未来天气
-    public static final String FORECAST_URL = "https://api.seniverse.com/v3/weather/daily.json?key=SM1jkVpoja_DPR9gF&location=%s&language=zh-Hans&unit=c&start=0&days=%d";
+    public static final String FORECAST_URL = String.format("https://api.seniverse.com/v3/weather/daily.json?key={}&location=%s&language=zh-Hans&unit=c&start=0&days=%d",API_KEY};
 
     // 生活指数
-    public static final String LIFEINDEX_URL = "https://api.seniverse.com/v3/life/suggestion.json?key=SM1jkVpoja_DPR9gF&location=%s&language=zh-Hans";
+    public static final String LIFEINDEX_URL = String.format("https://api.seniverse.com/v3/life/suggestion.json?key={}&location=%s&language=zh-Hans",API_KEY};
 
     // 数据库
     public static final String DB_ALIAS = "RecordBean";
